ef1015c30c519bf7b676e16e011e14f922b3a3a1
Sergii Volodarskyi <47503455+svolodarskyi@users.noreply.github.com>
2024-11-13 14:29:45 -0700

    Update main.js

--- src/main.js
+++ src/main.js
@@ -3,50 +3,64 @@ import fetchData from "./js/pixabay-api";
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const input = document.querySelector(".search-field");
-const button = document.querySelector("button");
+const form = document.querySelector(".searching-form");
 const galleryContainer = document.querySelector(".gallery");
 const loaderIconContainer = document.querySelector(".loader-icon-container");
 const loadMore = document.querySelector(".js-load-more");
-
+const buttonContainer = document.querySelector(".button-container");
 
 let page = 1;
 let searchWord;
+let totalHits;
 
-button.addEventListener("click", handleClick);
+form.addEventListener("submit", handleSubmit);
 loadMore.addEventListener("click", handleLoadMore)
 
-async function handleClick(event) {
+async function handleSubmit(event) {
     event.preventDefault();
     
-    loadMore.classList.add('load-more-hidden');
+    buttonContainer.classList.add('load-more-hidden');
     // Reset gallery
     galleryContainer.innerHTML = "";
-    loaderIconContainer.classList.toggle("loader");
-    searchWord = input.value.trim();
     
+    searchWord = event.target.elements.search.value.trim();
+    
+    if (!searchWord) {
+        iziToast.error({
+            message: "Please enter a keyword into the search field",
+            position: "topRight",
+            timeout: 1500
+        });
+        return;
+    }
+
+    loaderIconContainer.classList.toggle("loader");
+
     try {
         const { data } = await fetchData(searchWord, page);
         const imagesArr = data.hits;
 
-        if (imagesArr.length === 0 || searchWord === '') {
+        if (imagesArr.length === 0) {
             iziToast.error({
                 message: "Sorry, there are no images matching your search query. Please try again!",
                 position: "topRight",
+                timeout: 1500
             });
         } else {
+            totalHits = data.totalHits
             const galleryMarkup = generateMarkup(imagesArr);
             renderImagesingallery(galleryMarkup, galleryContainer);
-            loadMore.classList.remove('load-more-hidden');
+            buttonContainer.classList.remove('load-more-hidden');
         }
     } catch (error) {
         iziToast.error({
             message: error.message,
             position: "topRight",
+            timeout: 5000
         });
     } finally {
         loaderIconContainer.classList.toggle("loader");
-        input.value = "";
+        event.target.elements.search.value = "";
     }
 }
 
@@ -55,31 +69,33 @@ async function handleLoadMore(event) {
     event.preventDefault(); 
     loadMore.disabled = true;
 
+    if (totalHits <= page * 15) {
+        buttonContainer.classList.add('load-more-hidden');
+        iziToast.info({
+            message: "We're sorry, but you've reached the end of search results.",
+            position: "topRight",
+        });
+        return;
+    }
+
     page += 1;
     loaderIconContainer.classList.toggle("loader");
 
     try {
         const { data } = await fetchData(searchWord, page);
         const imagesArr = data.hits;
+        totalHits = data.totalHits;
+        const galleryMarkup = generateMarkup(imagesArr);
 
-        if (data.totalHits <= page * 15) {
-            loadMore.classList.add('load-more-hidden');
-            iziToast.info({
-                message: "We're sorry, but you've reached the end of search results.",
-                position: "topRight",
-            });
+        renderImagesingallery(galleryMarkup, galleryContainer);
 
-        } else {
-            const galleryMarkup = generateMarkup(imagesArr);
-            renderImagesingallery(galleryMarkup, galleryContainer);
-            const card = document.querySelector(".photo-card");
-            const cardHeight = card.getBoundingClientRect().height;
-            window.scrollBy({
+        const card = document.querySelector(".photo-card");
+        const cardHeight = card.getBoundingClientRect().height;
+        window.scrollBy({
                 left: 0,
                 top: cardHeight*2,
                 behavior: "smooth"
         })
-        }
     } catch (error) {
         iziToast.error({
             message: error.message,
@@ -87,9 +103,7 @@ async function handleLoadMore(event) {
         });
     } finally {
         loaderIconContainer.classList.toggle("loader");
-        loadMore.disabled = false;
-        
+        loadMore.disabled = false; 
     }
-    
 }
 
